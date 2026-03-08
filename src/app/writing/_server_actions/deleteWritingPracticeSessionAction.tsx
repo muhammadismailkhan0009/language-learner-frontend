f3ee69db6779ddc250b4057dc6348ac29f4a1b7c
muhammadismailkhan0009 <muhammadismailkhan0009@gmail.com>
2026-03-08 04:43:13 +0100

    updated response issue

--- src/app/writing/_server_actions/deleteWritingPracticeSessionAction.tsx
+++ src/app/writing/_server_actions/deleteWritingPracticeSessionAction.tsx
@@ -4,5 +4,5 @@ import { deleteWritingPracticeSession } from "@/lib/serverBackedApiCalls";
 
 export default async function deleteWritingPracticeSessionAction(sessionId: string): Promise<boolean> {
     const response = await deleteWritingPracticeSession(sessionId);
-    return response.status === 200;
+    return response.status === 200 || response.status ===204;
 }
